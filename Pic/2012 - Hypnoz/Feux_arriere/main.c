a41b6a2d9395b187eacf8d9231a179a09a20984d
bruno.lavaud <bruno.lavaud@4a81d713-3812-46e6-af9e-50336874a00c>
2012-02-12 13:42:14 +0000

    Encapsulation des fonctions de debug sur la carte feux arrières

--- Pic/2012 - Hypnoz/Feux_arriere/main.c
+++ Pic/2012 - Hypnoz/Feux_arriere/main.c
@@ -9,12 +9,14 @@
 //      Version 1.01  - BLD - 27/11/2011 -> renommage variable //    
 //	    Version 1.02  - BLD - 27/11/2011 -> trace debug        //
 //		Version 1.03  - BLD - 18/12/2011 -> recalibrage timer  //
+//      Version 1.04  - BLD - 12/02/2012 -> encapsulation debug//
 //			                                                   //
 /////////////////////////////////////////////////////////////////
 
 #include <18F258.h>
 #include <can-18xxx8.c>
 #include <CAN_id.h>
+#include <debug.h>
 
 //#define CAN_USE_EXTENDED_ID         FALSE
 
@@ -24,16 +26,8 @@
 #define CLIGN_R         PIN_A2
 #define FEUX_STOP       PIN_A3
 
-//Mode debug commenter la ligne pour l'enlever
-#define DEBUG 1
-#define TRACE 1
-#define TRACE_CAN 1
+#fuses HS,NOPROTECT,NOLVP,WDT
 
-#ifdef DEBUG
-	#fuses HS,NOPROTECT,NOLVP,NOWDT
-#else
-	#fuses HS,NOPROTECT,NOLVP,NOWDT
-#endif
 #use delay(clock=20000000)
 #use rs232(baud=115200,xmit=PIN_C6,rcv=PIN_C7)
 
@@ -82,6 +76,7 @@ void isr_timer2()
 void main()
 {
 	//initialisation du PIC
+	LOG_DEBUG(TRACE_EXEC||TRACE_ALL,"Entering in main fonction",sec,ms)
 	setup_adc_ports(NO_ANALOGS);        //on gère toutes les entrées comme étant de type numérique
 
 	enable_interrupts(INT_TIMER2);      //configuration des interruptions
@@ -92,29 +87,11 @@ void main()
 	can_set_baud();						//obsolète à priori à tester
 	restart_wdt();
 
-	#ifdef DEBUG
-	   // Mise en évidence d'un problème lié au Watchdog
-   	   switch ( restart_cause() )
-       {
-          case WDT_TIMEOUT:
-          {
-             printf("\r\nRestarted processor because of watchdog timeout!\r\n");
-             break;
-          }
-          case NORMAL_POWER_UP:
-          {
-             printf("\r\nNormal power up! PIC initialized \r\n");
-             break;
-          }
-       }
-       restart_wdt();
-    #endif
-
+	CHECK_PWUP						    //on vérifie que le démarrage est du à une mise sous tension et non un watchdog
 
 	//  BOUCLE DE TRAVAIL
 	while(TRUE)
 	{
-		
 		restart_wdt();
 		listenCAN();
 
@@ -133,28 +110,37 @@ void listenCAN()        // Fonction assurant la réception des messages sur le CA
 	int32 rxId;
 	int8 rxData[8];
 	int8 rxLen;
+	int r=0;			// flag assurant la bonne lecture de la donnée sur le CAN
+
+	LOG_DEBUG(TRACE_EXEC||TRACE_ALL,"Entering in ListenCAN",sec,ms)
 
 	if(can_kbhit())                                // Une donnée est présente dans le buffer de réception du CAN
 	{
+		LOG_DEBUG(TRACE_CAN,"Something hit the CAN bus",sec,ms)	
 		if(can_getd(rxId,&rxData[0],rxLen,rxStat)) // on récupère le message
 		{
+			LOG_DEVELOPMENT_LD(TRACE_CAN||TRACE_ALL,"CAN RX - ID ",rxId,sec,ms)
+			r=1;								   // On a effectivement lu quelque chose sur le CAN
 			switch(rxId)                           // en fonction de l'id on le traite spécifiquement
 			{
 				case BRAKE_ORDER:
 				{		
 					brake=rxData[0];			   // On change l'état des feux stops
+					LOG_TESTING_D(TRACE_ALL||TRACE_BRAKE_LIGHT,"Brake light status incomming from the CAN is ",rxData[0],sec,ms)
 					break;
 				}
 				case BLINK_ORDER_LEFT:
 				{		
 					blinkl_ack=1;                  // On place le flag à 1 pour envoyer l'accusé de réception ultérieurement
 					blink_left=rxData[0];          // On change l'état duclign_msotant gauche
+					LOG_TESTING_D(TRACE_ALL||TRACE_ALL_LIGHT||TRACE_ALL_LIGHT_R||TRACE_BLINK_BACK||TRACE_BLINK_LB,"Blink (left) status incomming from the CAN is ",rxData[0],sec,ms)
 					break;
 				}
 				case BLINK_ORDER_RIGHT:
 				{
                     blinkr_ack=1;                  // On place le flag à 1 pour envoyer l'accusé de réception ultérieurement
 					blink_right=rxData[0];         // On change l'état duclign_msotant droit
+					LOG_TESTING_D(TRACE_ALL||TRACE_ALL_LIGHT||TRACE_ALL_LIGHT_R||TRACE_BLINK_BACK||TRACE_BLINK_RB,"Blink (right) status incomming from the CAN is ",rxData[0],sec,ms)
 					break;
 				}
 				case LIGHT_ORDER:
@@ -168,29 +154,12 @@ void listenCAN()        // Fonction assurant la réception des messages sur le CA
 					{
 						light=1;				   // On allume les feux
 					}
+					LOG_TESTING_D(TRACE_ALL||TRACE_ALL_LIGHT||TRACE_ALL_LIGHT_R||TRACE_LIGHT_BACK,"Light status incomming from the CAN is ",rxData[0],sec,ms)
+					LOG_TESTING_D(TRACE_ALL||TRACE_ALL_LIGHT||TRACE_ALL_LIGHT_R||TRACE_LIGHT_BACK,"Light status is now ",light,sec,ms)
 					break;
 				}
 			}
-		#ifdef TRACE
-			restart_wdt();
-			tmp=ms+1000*sec;
-			if((rxId==BRAKE_ORDER || rxId==BLINK_ORDER_LEFT || rxId==BLINK_ORDER_RIGHT || rxId==LIGHT_ORDER) && rxLen>=1)
-			{
-				printf("\r\n [%Lu] - CAN RX - ID=%u - DATA=%u", tmp,rxId,rxData[0]);
-			}
-		#endif
-		#ifdef TRACE_CAN
-			tmp=ms+1000*sec;
-			printf("\r\n [%Lu] - CAN_DEBUG - BUFF=%u - ID=%u - LEN=%u - OVF=%u", tmp,rxStat.buffer, rxId, rxLen, rxStat.err_ovfl);
-		#endif
-		}
-		else
-		{
-		#ifdef TRACE_CAN
-			restart_wdt();
-	        tmp=ms+1000*sec;
-			printf("[%Lu] - CAN_DEBUG - FAIL on can_getd function", tmp);
-		#endif
+			LOG_LISTEN_CAN(r,rxStat,rxId,rxLen,sec,ms)
 		}
 	}
 }
@@ -198,6 +167,8 @@ void listenCAN()        // Fonction assurant la réception des messages sur le CA
 #inline
 void internalLogic() //Fonction en charge de la gestion des fonctionnalités de la carte
 {
+
+	LOG_DEBUG(TRACE_EXEC||TRACE_ALL,"Entering in internalLogic",sec,ms)
 	//gestion des feux stops et arrières
 	output_bit(FEUX_STOP, brake);                         // si brake est à true, allumer les feux stop, et inversement
 	output_bit(FEUX, light);                              // si light est à true, allumer les feux arrière, et inversement
@@ -205,16 +176,12 @@ void internalLogic() //Fonction en charge de la gestion des fonctionnalités de l
 
 	if(clign_ms >= 650)                                   // 650 ms = +- 1.5Hz après l'activation du timer = frequence desclign_msotants
 	{
+		LOG_TESTING(TRACE_ALL||TRACE_EXEC||TRACE_BLINK_BACK,"Blink status has been toggled",sec,ms)
 		clign_ms = 0;                                     // repartir pour 500 ms
 		blink_status = !blink_status ;                    // changer l'état d'allumage desclign_msotants
 		
 		output_bit(CLIGN_R, blink_status & blink_right); // allumer ou éteindre le clignotant droit s'il est activé
 		output_bit(CLIGN_L, blink_status & blink_left);  // allumer ou éteindre le clignotant gauche s'il est activé
-		#ifdef TRACE
-			restart_wdt();
-	        tmp=ms+1000*sec;
-			printf("[%Lu] - INFO - Blink status has been toggled", tmp);	
-		#endif
 	}
 }
 
@@ -222,58 +189,40 @@ void internalLogic() //Fonction en charge de la gestion des fonctionnalités de l
 void sendCAN()
 {
 	int r;
+
+	LOG_DEBUG(TRACE_EXEC||TRACE_ALL,"Entering in SendCAN",sec,ms)
+
 	if(blinkl_ack==1)
 	{
+		LOG_TESTING(TRACE_ALL||TRACE_EXEC||TRACE_BLINK_BACK,"An ACK paquet for blink_left is still remaining",sec,ms)
 		if(can_tbe()) // On vérifie que le buffer d'emission est libre
 		{
+			LOG_DEBUG(TRACE_ALL||TRACE_EXEC||TRACE_CAN,"CAN buffer emit is empty. Entering in emiting process for blink_left ",sec,ms)
 			r=can_putd(BLINK_LEFT_BACK_ACK,null,0,0,false,false); //emission de l'accusé de réception
 			blinkl_ack=0; // on a plus besoin d'envoyer l'accusé de réception
-			#ifdef TRACE_CAN
-				restart_wdt();
-				tmp=1000*sec+ms;
-				if (r != 0xFF)
-				{
-					printf("\r\n [%Lu] - CAN TX - %u - ID=%u - LEN=%u - DATA=%u",tmp, r, BLINK_LEFT_BACK_ACK,1,blink_left);
-				}
-				else
-					printf("\r\n [%Lu] - CAN_DEBUG - FAIL on can_putd function \r\n",tmp);
-			#endif
+			LOG_SEND_CAN(r,BLINK_LEFT_BACK_ACK,0,sec,ms)
 		}
 	}
 	else if(blinkr_ack==1)
 	{
+		LOG_TESTING(TRACE_ALL||TRACE_EXEC||TRACE_BLINK_BACK,"An ACK paquet for blink_right is still remaining",sec,ms)
 		if(can_tbe()) // On vérifie que le buffer d'emission est libre
 		{
+			LOG_DEBUG(TRACE_ALL||TRACE_EXEC||TRACE_CAN,"CAN buffer emit is empty. Entering in emiting process for blink_right ",sec,ms)
 			r=can_putd(BLINK_RIGHT_BACK_ACK,null,0,0,false,false); //emission de l'accusé de réception
 			blinkr_ack=0; // on a plus besoin d'envoyer l'accusé de réception
-			#ifdef TRACE_CAN
-				restart_wdt();
-				tmp=1000*sec+ms;
-				if (r != 0xFF)
-				{
-					printf("\r\n [%Lu] - CAN TX - %u - ID=%u - LEN=%u - DATA=%u",tmp, r, BLINK_RIGHT_BACK_ACK,1,blink_right);
-				}
-				else
-					printf("\r\n [%Lu] - CAN_DEBUG - FAIL on can_putd function \r\n",tmp);
-			#endif
+			LOG_SEND_CAN(r,BLINK_RIGHT_BACK_ACK,0,sec,ms)
 		}
 	}
 	else if(light_ack==1)
 	{
+		LOG_TESTING(TRACE_ALL||TRACE_EXEC||TRACE_BLINK_BACK,"An ACK paquet for light is still remaining",sec,ms)
 		if(can_tbe()) // On vérifie que le buffer d'emission est libre
 		{
+			LOG_DEBUG(TRACE_ALL||TRACE_EXEC||TRACE_CAN,"CAN buffer emit is empty. Entering in emiting process for light ",sec,ms)
 			r=can_putd(LIGHT_BACK_ACK,null,0,0,false,false); //emission de l'accusé de réception
 			light_ack=0; // on a plus besoin d'envoyer l'accusé de réception
-			#ifdef TRACE_CAN
-				restart_wdt();
-				tmp=1000*sec+ms;
-				if (r != 0xFF)
-				{
-					printf("\r\n [%Lu] - CAN TX - %u - ID=%u - LEN=%u - DATA=%u",tmp, r, LIGHT_BACK_ACK,1,light);
-				}
-				else
-					printf("\r\n [%Lu] - CAN_DEBUG - FAIL on can_putd function \r\n",tmp);
-			#endif
+	     	LOG_SEND_CAN(r,LIGHT_BACK_ACK,0,sec,ms)
 		}
 	}
 }
