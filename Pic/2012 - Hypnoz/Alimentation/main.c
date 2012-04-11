b6c41c18e9bd45104724fd639ad78f3b2e6eb198
bruno.lavaud <bruno.lavaud@4a81d713-3812-46e6-af9e-50336874a00c>
2012-04-11 08:35:03 +0000

    Test unitaire et deboguage carte alimentation OK

--- Pic/2012 - Hypnoz/Alimentation/main.c
+++ Pic/2012 - Hypnoz/Alimentation/main.c
@@ -13,12 +13,13 @@
 //		Version 1.05 - BLD - 18/12/2011 -> recalibrage timer   //      
 //		Version 1.06 - BLD - 11/12/2011 -> encapsulation debug //  
 //		Version 1.07 - BLD - 20/03/2012 -> patch extended id   // 
+//		Version 2.00 - BLD - 11/04/2012 -> test unitaire OK    //
 //                                                             //                                                
 /////////////////////////////////////////////////////////////////
 
 #define CAN_USE_EXTENDED_ID         FALSE
 
-#include <18F258.h>
+#include <18F2580.h>
 #include <can-18xxx8.c>
 #include <CAN_id.h>
 #include <debug.h>
@@ -32,7 +33,7 @@
 // Rapport du pont diviseur de tension
 #define ku	3								   // il faut multiplier la tension mesurée par 3
 
-#fuses HS,NOPROTECT,NOLVP,WDT
+#fuses HS,NOPROTECT,NOLVP,NOWDT
 
 
 #use delay(clock=20000000)
@@ -117,7 +118,7 @@ void main()
 #inline
 void internalLogic() //Fonction en charge de la gestion des fonctionnalités de la carte
 {
-	unsigned int16 data;		
+	 int16 data;		
 
 	LOG_DEBUG(TRACE_EXEC||TRACE_ALL,"Entering in internalLogic",sec,ms)
 
@@ -126,11 +127,12 @@ void internalLogic() //Fonction en charge de la gestion des fonctionnalités de l
 	
 	if(adc_done==1)
 	{
+		delay_ms(200);
 		data=read_adc(ADC_READ_ONLY);			  // On lit la valeur correspondante
-		charge=(int16)(data*4*ku);				  // voltage contient désormais la charge de la batterie en mV
+		charge=(int16)(data*20*ku);				  // voltage contient désormais la charge de la batterie en mV
 		adc_done=0;								  // On remet à zéro la lecture de l'ADC
 		read_adc(ADC_START_ONLY);				  // On lance une nouvelle acquisition
-		LOG_TESTING_LU(TRACE_ALL||TRACE_CHARGE,"ADC INFO - Battery voltage ",charge,sec,ms)
+		LOG_TESTING_LD(TRACE_ALL||TRACE_CHARGE,"ADC INFO - Battery voltage ",charge,sec,ms)
 	}
 }
 
@@ -143,7 +145,7 @@ void sendCAN()
 
 	if(battery_reemit_ms>=TR_BATTERY)
 	{
-		LOG_DEVELOPMENT(TRACE_EXEC||TRACE_ALL,"Reemit battery time is over",sec,ms)
+//		LOG_DEVELOPMENT(TRACE_EXEC||TRACE_ALL||TRACE_CHARGE,"Reemit battery time is over",sec,ms)
 		if(can_tbe()) // On vérifie que le buffer d'emission est libre
 		{
 			LOG_DEBUG(TRACE_ALL||TRACE_EXEC||TRACE_CAN,"CAN buffer emit is empty. Entering in emiting process for battery status",sec,ms)
